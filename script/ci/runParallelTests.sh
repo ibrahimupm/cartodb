c6c56aa2aa2ac4504f1b855f67d4b9360bcda564
Alberto Miedes Garcés <albertomg994@gmail.com>
2021-02-05 12:03:19 +0100

    Try parallel test

--- script/ci/runParallelTests.sh
+++ script/ci/runParallelTests.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# Copy database.yml
+cp /cartodb/config/database.ci.yml /cartodb/config/database.yml
+
 # Init Builder
 cd /cartodb
 mkdir -p /cartodb/log && chmod -R 777 /cartodb/log
@@ -10,13 +13,29 @@ psql -h localhost -U postgres template_postgis -c 'CREATE EXTENSION IF NOT EXIST
 REDIS_PORT=6335 RAILS_ENV=test bundle exec rake cartodb:test:prepare
 cd -
 
+# Create additional databases
+bundle exec rake parallel:create
+
+# Copy development schema
+bundle exec rake parallel:prepare
+
+# Run migrations
+bundle exec rake parallel:migrate
+
+# Setup environment from scratch
+bundle exec rake parallel:setup
+
+# Run parallel tests
+bundle exec rake parallel:spec['spec\/models']
+
 # [OK] bundle exec rspec spec/commands
 
-# [?]
+# [OK]
 # bundle exec rspec \
 #   spec/models/carto/user_spec.rb \
 #   spec/models/carto/user_table_spec.rb \
 #   spec/models/table_spec.rb
 
-bundle exec rspec \
-  spec/requests/superadmin
+# [?]
+# bundle exec rspec \
+#   spec/requests/superadmin
