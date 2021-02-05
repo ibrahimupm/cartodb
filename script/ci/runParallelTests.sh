4bcd7acf684158f7e4e2528be4fa5f2ebfda3509
Alberto Miedes Garcés <albertomg994@gmail.com>
2021-02-05 10:49:41 +0100

    Remove some steps

--- script/ci/runParallelTests.sh
+++ script/ci/runParallelTests.sh
@@ -1,35 +1,23 @@
 #!/bin/bash
 
+set -e
+
 WORKERS=${1:-22}
 
-syntax_errors=0
-# check syntax of modified Ruby files before running tests 
-for i in $(git diff --name-only origin/master -- "*.rb" ); 
-    do echo $i; ruby -c $i || ((syntax_errors=syntax_errors+1)); 
-done
-if [ $syntax_errors -gt 0 ]
-then
-    echo "Syntax Errors": $syntax_errors;
-    exit 1;
-fi
-
 # init builder
-script/ci/init.sh || exit 1
+script/ci/init.sh
 
 # BACKEND PARALLEL
-script/ci/generateSpecFull.sh || exit 1
+script/ci/generateSpecFull.sh
 
 # CLEANER
-script/ci/cleaner.sh || exit 1
+script/ci/cleaner.sh
 
 # WRAPPER
-script/ci/wrapper.sh $WORKERS || exit 1
+script/ci/wrapper.sh $WORKERS
 
 # TESTS
-time parallel -j $WORKERS -a parallel_tests/specfull.txt 'script/ci/executor.sh {} {%} {#}' || exit 1
-
-# SECOND TRY
-script/ci/secondTry.sh || exit 1
+time parallel -j $WORKERS -a parallel_tests/specfull.txt 'script/ci/executor.sh {} {%} {#}'
 
 # REPORTER
-script/ci/reporter.sh || exit 1
+script/ci/reporter.sh
